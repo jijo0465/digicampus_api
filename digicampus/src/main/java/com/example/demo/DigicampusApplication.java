d647f7de73c152b9ea5161cb0c319d027da241b9
jijo0465 <jijo0465@gmail.com>
2020-04-11 04:11:56 -0700

    Initial Commit

---
+++ digicampus/src/main/java/com/example/demo/DigicampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigicampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigicampusApplication.class, args);
+	}
+
+}
